ea68ce7fb2db36388a15a24a35349bf7dd527060
profdanielrsousa <114257220+profdanielrsousa@users.noreply.github.com>
2024-02-26 23:18:16 -0300

    Versão inicial.

---
+++ ex2_pisca_led.ino
@@ -0,0 +1,15 @@
+/* SEGUNDO PROGRAMA (PISCA LED MODIFICADO)
+   DANIEL RODRIGUES DE SOUSA 26/02/2024 */
+
+#define	LED	13 //pino onde está ligado o LED
+
+void setup()
+{
+  pinMode(LED, OUTPUT); //direção do pino (saída)
+}
+
+void loop()
+{
+  digitalWrite(LED,!digitalRead(LED));	//lê o estado atual do pino e troca o estado
+  delay(1000);            		//delay de 1 segundo  
+}
