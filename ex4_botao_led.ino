af0fdca1e62dda9bb9f40a324a5c2e0fe4acc728
profdanielrsousa <114257220+profdanielrsousa@users.noreply.github.com>
2024-04-28 18:46:55 -0300

    Versão inicial.

--- ex4_botao_led.ino
+++ ex4_botao_led.ino
@@ -1,23 +1,25 @@
 /* QUARTO PROGRAMA (BOTÃO E LED)
-   DANIEL RODRIGUES DE SOUSA 02/03/2024 */
+   DANIEL RODRIGUES DE SOUSA 02/03/2024 */ 
 
-#define LED	   8
-#define BOTAO	9
+#define LED   8 // Define a constante LED no pino 8
+#define BOTAO 9 // Define a constante BOTAO no pino 9
 
 void setup()
 {
-  pinMode(LED, OUTPUT);
-  pinMode(BOTAO, INPUT);
+  // Configura as portas de saída e entrada para o LED e botão respectivamente
+  pinMode(LED, OUTPUT);   // Define o pino do LED como saída
+  pinMode(BOTAO, INPUT);  // Define o pino do botão como entrada
 }
 
 void loop()
 {
-  if(digitalRead(BOTAO) == 0)
+  if(digitalRead(BOTAO) == 0) // Verifica se o botão está pressionado (0) ou não (1)
+
   {
-    digitalWrite(LED, HIGH);
+    digitalWrite(LED, HIGH);  // Se estiver pressionado, acende o LED
   }
   else
   {
-    digitalWrite(LED, LOW);
+    digitalWrite(LED, LOW);   // Se não estiver pressionado, apaga o LED
   }
 }
